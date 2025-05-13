55f04e7f1ca4840deb594788b6486b39d309df8b
Mandeep Gahlawat <mandeepgahlawat@Mandeeps-MacBook-Air.local>
2025-05-13 15:38:52 +0530

    added analytics and speed insights

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Send from './components/Send';
 import Receive from './components/Receive';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/send' element={<Send/>}/>
-          <Route path='/receive' element={<Receive/>}/>
-        </Routes>
-      </BrowserRouter>
-
+      <SpeedInsights>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/send' element={<Send />} />
+            <Route path='/receive' element={<Receive />} />
+          </Routes>
+        </BrowserRouter>
+      </SpeedInsights>
     </div>
   );
 }
